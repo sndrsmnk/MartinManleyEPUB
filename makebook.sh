80cc1da690a2571d318e787877403418777e4ddf
Sander Smeenk <github@freshdot.net>
2013-08-28 19:57:17 +0200

    This compiles into an EPUB.

--- makebook.sh
+++ makebook.sh
@@ -5,6 +5,22 @@ if [ ! -x "$zip" ]; then
     echo "zip utility not found."
     exit 1
 fi
+echo "zip utility found as '$zip'"
+
+kindlegen=$(which kindlegen)
+if [ ! -x "$kindlegen" ]; then
+    echo "kindlegen utility not in path."
+    kindlegen="$HOME/KindleGen/kindlegen"
+    if [ ! -x "$kindlegen" ]; then
+        echo "kindlegen utility not found."
+        exit 1
+    fi
+fi
+echo "kindlegen utility found as '$kindlegen'"
+
 
 cd epub_root
-zip -r ../Martin_Manley-My_life_and_death.epub .
+zip -qr ../../Martin_Manley-My_life_and_death.epub .
+
+cd ../../
+~/KindleGen/kindlegen Martin_Manley-My_life_and_death.epub
